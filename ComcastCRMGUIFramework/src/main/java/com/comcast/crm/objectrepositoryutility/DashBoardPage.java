0bd0f66cde55206464d4157f77938f1105ab4376
vidya035 <vidyasm035@gmail.com>
2026-04-02 12:41:08 +0530

    DashBoard, Added new POM class

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/DashBoardPage.java
@@ -0,0 +1,26 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashBoardPage {
+	WebDriver driver;
+
+	public DashBoardPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(linkText = "Dashboard")
+	private WebElement DashboarLink;
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public WebElement getDashboarLink() {
+		return DashboarLink;
+	}
+}
